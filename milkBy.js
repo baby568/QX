69726e05d29a69289e38db963044b4d9319813dd
baby568 <138785483+baby568@users.noreply.github.com>
2023-07-06 22:57:43 +0800

    Create milkBy.js
    奶由壁纸

---
+++ milkBy.js
@@ -0,0 +1,17 @@
+/******************************
+脚本功能：奶由壁纸
+脚本作者：baby568
+更新时间：2023-07-06
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+#奶由壁纸解锁vip
+^https:\/\/nz-api\.duitang\.com\/account\/me\/ url script-response-body 
+[mitm] 
+hostname = nz-api.duitang.com
+*******************************/
+var body = $response.body;
+var obj =  JSON.parse(body);
+obj.data.vip = true;
+body = JSON.stringify(obj);
+$done({body});
