306eac56c0f126738f23dc23307d4bb8ac205c8a
baby568 <138785483+baby568@users.noreply.github.com>
2023-07-06 23:12:59 +0800

    Create mint.js

---
+++ mint.js
@@ -0,0 +1,20 @@
+/******************************
+脚本功能：薄荷壁纸解锁vip
+脚本作者：baby568
+更新时间：
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+#薄荷壁纸解锁vip
+^https:\/\/mars-prod\.whalean\.com\/hestia-service\/api\/wpUser\/getUserInfo url script-response-body 
+[mitm] 
+hostname = mars-prod.whalean.com
+*******************************/
+var body = $response.body;
+var obj =  JSON.parse(body);
+
+obj.memberType = 1;
+obj.memberEndTime= 11111111111;
+
+body = JSON.stringify(obj);
+$done({body});
