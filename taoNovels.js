64df181587ec2fc9bb2fd4ecfad359fea6dbb438
baby568 <138785483+baby568@users.noreply.github.com>
2023-07-07 10:45:33 +0800

    Create taoNovels.js
    淘小说

---
+++ taoNovels.js
@@ -0,0 +1,17 @@
+/******************************
+脚本功能：淘小说解锁vip
+脚本作者：baby568
+更新时间：2023-07-07
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+#淘小说解锁vip
+^https:\/\/tybook\.ios\.itaoxiaoshuo\.com\/auth\/account url script-response-body 
+[mitm] 
+hostname = tybook.ios.itaoxiaoshuo.com
+*******************************/
+var obj =  JSON.parse($response.body);
+
+obj.isVip = obj.paymentUser = true;
+
+$done(JSON.stringify(obj));
