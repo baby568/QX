7cd4f70c5a84caaf802f1f60efc124f7219bf6eb
baby568 <138785483+baby568@users.noreply.github.com>
2023-07-08 23:25:36 +0800

    Update undulating.js

--- undulating.js
+++ undulating.js
@@ -1,1 +1,19 @@
+/******************************
+脚本功能：起伏解锁会员
+脚本作者：baby568
+更新时间：
+使用声明：️仅供学习交流, 🈲️商业用途
+*******************************
+[rewrite_local]
+#起伏解锁会员
+^https:\/\/api\.risingfalling\.com\/api\/vip\/detail url script-response-body 
+[mitm] 
+hostname = api.risingfalling.com
+*******************************/
 
+var obj =  JSON.parse($response.body);
+
+obj.body.isVip = true;
+obj.body.vipType = "VIP";
+
+$done(JSON.stringify(obj));
